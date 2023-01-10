c68e8e6df04cd83bd4859439442a5b1f7f2ae2dd
Kalpit Shah <kalpits21@gmail.com>
2023-01-10 21:08:34 +0530

    cors setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
